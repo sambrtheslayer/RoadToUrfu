9b0f94580e6df7ff4c971ca429c5cb37d2dba29d
Biosystem321 <36845473+Biosystem321@users.noreply.github.com>
2020-11-25 16:17:14 +0500

    Fixed bugs in code merging

--- app/src/main/java/com/example/urfu/Category.java
+++ app/src/main/java/com/example/urfu/Category.java
@@ -1,23 +1,10 @@
 package com.example.urfu;
 
 public class Category {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     private final int mId;
     private final String mName;
     private final String mAltName;
-=======
-    private int mId;
-    private String mName;
-    private String mAltName;
->>>>>>> parent of 4e14d25... Completed code review
-
-=======
-    private int mId;
-    private String mName;
-    private String mAltName;
->>>>>>> parent of 4e14d25... Completed code review
 
     public Category(int mId, String mName, String mAltName) {
         this.mId = mId;
