f0a5dd00195623418527d2341f40a328293e4a76
sambrtheslayer <sambrtheslayer@mail.ru>
2020-12-25 14:31:21 +0500

    Update bottom_sheet

--- app/src/main/java/com/example/urfu/ImageAdapter.java
+++ app/src/main/java/com/example/urfu/ImageAdapter.java
@@ -40,7 +40,7 @@ public class ImageAdapter extends BaseAdapter {
         ImageView imageView;
         if (convertView == null) {
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
+            imageView.setLayoutParams(new GridView.LayoutParams(220, 220));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(8, 8, 8, 8);
         } else {
